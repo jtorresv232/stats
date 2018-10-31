79503fc055190c2d36e392d59cfa3aa4039ce548
nilton <nilton.steveen@gmail.com>
2018-10-31 15:34:51 -0500

    libraries updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import {StatsRoutingModule,routingComponents} from './routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {HttpService} from './http.service';
-import { ToastModule } from './typescripts/pro/alerts/toast/toast.module';
+import { ToastModule } from 'ng-uikit-pro-standard';
 import { ConfiguracionComponent } from './configuracion/configuracion.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { StatisticsComponent2 } from './statistics2/statistics2.component';
 import { ToolbarComponent } from './layout/toolbar/toolbar.component';
-import { MDBBootstrapModule } from './typescripts/free';
-import { MDBBootstrapModulePro } from './typescripts/pro/index';
 import { SidenavComponent } from './layout/sidenav/sidenav.component';
 import { TabBarComponent} from './layout/tab-bar/tab-bar.component';
 import { GruposComponent } from './grupos/grupos.component';
-import { MDBSpinningPreloader } from './typescripts/pro/index';
 import { SabiasQueComponent } from './sabias-que/sabias-que.component';
 import { ActividadesComponent } from './actividades/actividades.component';
 import { TextosComponent } from './textos/textos.component';
@@ -52,12 +52,10 @@ import {AdminAuthService} from './admin-auth.service';
     HttpClientModule,
     ReactiveFormsModule,
     ToastModule.forRoot(),
-    MDBBootstrapModule.forRoot(),
-    MDBBootstrapModulePro.forRoot(),
+    MDBBootstrapModulesPro.forRoot(),
     StatsRoutingModule,
     ToastModule.forRoot()
-  ],
-  schemas: [ NO_ERRORS_SCHEMA ],
+  ],  
   providers: [HttpService, AuthGuardService, AdminAuthService, UserService, MDBSpinningPreloader],
   bootstrap: [AppComponent]
 })
