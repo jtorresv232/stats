11b1c70676d4091626f46c299089bb41ed9ab3ed
Andres Ruiz Graciano <aandres.r98@gmail.com>
2018-11-09 17:28:59 -0500

    first chart working perfectly

--- src/app/statistics-admin/statistics-admin.component.ts
+++ src/app/statistics-admin/statistics-admin.component.ts
@@ -7,10 +7,40 @@ import { AdministrativoService } from '../service/administrativo.service';
   styleUrls: ['./statistics-admin.component.scss']
 })
 export class StatisticsAdminComponent implements OnInit {
-  public optionsCategories: Array<any> = [];
-  public optionsStudent: Array<any> = [];
+  public categoryOptions: Array<any> = [];
+  public studentOptions: Array<any> = [];
+
+  enabledBarChart = false;
+  barType = 'bar';
+  public chartColors: Array<any> = [
+    {
+        backgroundColor: 'rgba(255,0,0,0.2)',
+        borderColor: 'rgba(255,0,0,1)',
+        borderWidth: 2,
+        pointBackgroundColor: 'rgba(255,0,0,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(255,0,0,1)'
+    }
+  ];
+  barData: any[] = [];
+  barDatasets: Array<any> = [];
+  barLabels: Array<any> = [];
+  arrayBarDatasets: Array<any> = [];
+  chartOptions: any = {
+    responsive: true,
+    scales: {
+      yAxes: [{
+          ticks: {
+              beginAtZero: true
+          }
+      }]
+  }
+  };
 
   idCategories: any;
+  initialDate: string;
+  endDate: string;
   idStudent: any;
 
   constructor(private adminService: AdministrativoService) { }
@@ -19,7 +49,7 @@ export class StatisticsAdminComponent implements OnInit {
     this.adminService.getStudents(5).subscribe(
       data => {
         data.entity.forEach(student => {
-          this.optionsStudent.push(
+          this.studentOptions.push(
             {
               value: student.id,
               label: student.nombres + ' ' + student.apellidos
@@ -33,7 +63,7 @@ export class StatisticsAdminComponent implements OnInit {
     this.adminService.getCategories().subscribe(
       data => {
         data.entity.forEach(category => {
-          this.optionsCategories.push(
+          this.categoryOptions.push(
             {
               value: category.id,
               label: category.nombre
@@ -45,19 +75,32 @@ export class StatisticsAdminComponent implements OnInit {
 
       }
     );
-    /*this.optionsStudent.push({ value: 1, label: 'Esto si lo inserta' });
-    this.optionsStudent.push({ value: 5, label: 'Esto si lo ' });
-    this.optionsStudent.push({ value: 2, label: 'Esto ' });
-    this.optionsCategories = [
-      { value: 1, label: 'Armonioso' },
-      { value: 2, label: 'Aislamiento' },
-      { value: 3, label: 'Felicidad' },
-    ];*/
   }
 
-  mostarGrafico() {
-    console.log(this.idStudent);
-    console.log(this.idCategories);
+  mostarGrafico(opt: number) {
+    this.enabledBarChart = false;
+    this.barDatasets = [];
+    this.barDatasets.push({data: [], label: 'Repeticiones '});
+    this.barLabels = [];
+    if (opt === 1) {
+      this.adminService.getStudentStatistics(5, this.idStudent, this.initialDate, this.endDate, this.idCategories).subscribe(
+        data => {
+          data.entity.forEach(value => {
+            this.barDatasets[0]['data'].push(value.repeticiones);
+            this.barLabels.push(value.categoria__nombre);
+          });
+         // this.barDatasets['data'] = this.barData;
+        this.enabledBarChart = true;
+        },
+        error => {
+        }
+      );
+    }
   }
 
+  public chartClicked(e: any): void {
+  }
+
+  public chartHovered(e: any): void {
+  }
 }
